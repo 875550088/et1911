e5642385a184b4de5701cc996a12fdb7d38a5501
et1911 <et1911@qq.com>
2020-05-19 14:57:46 +0800

    提交spring-mvc -02 -03工程

---
+++ springmvc-02/src/main/java/com/etoak/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.etoak.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloController implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+			
+		//1.获取参数
+		String name = request.getParameter("name");
+		//2.调取service服务器
+		
+		//3.返回页面
+		ModelAndView mv = new ModelAndView();
+		//webapp/pages/hello.jsp
+		mv.setViewName("hello");
+		//想request域传值
+		mv.addObject("result","hello" + name);
+		
+		return mv;
+	}
+
+}
